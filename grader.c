549a1886286554edf25444fa9de8d0b37a1c44f6
Ungku Amer <46221135+ungkuamer@users.noreply.github.com>
2022-10-27 16:08:50 +0100

    Add files via upload

---
+++ grader.c
@@ -0,0 +1,35 @@
+
+/*
+ * Template file for the solution to part 1
+ * Use the included print statements in your code at appropriate times.
+ * Do not adjust the format of the print statements.
+ * Do not submit a final solution with additional print statements.
+ */
+
+#include <stdio.h>
+
+int main( int argc, char **argv ) {
+
+  // command line arguments
+  printf("No input file name given. Exiting.\n");
+  printf("Input file does not exist. Exiting.\n");
+  
+  // reading from file
+  printf("Input file. Opening.\n");
+  printf("Input file. Closing.\n");
+
+  // data processing
+  printf("Checking data.\n");
+  printf("Found an invalid student id: %d. Exiting.\n",id); // requires student id
+  printf("Correcting student %d grade %d\n",id,grade); // requires student id and relevant grade
+
+  // compute averages
+  printf("Computing averages.\n");
+
+  // writing to file
+  printf("Output file. Opening.\n");
+  printf("Output file. Closing.\n");
+
+  return 0;
+}
+
